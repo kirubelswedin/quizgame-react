2094f1c29e58e5e89bf045e4d5c54a759d68c94b
kirubelswedin <kirubelswedin@gmail.com>
2025-02-03 17:32:24 +0100

    Fix import casing for starsWrapper.css in StarsWrapper component

--- src/components/ui/starsWrapper.tsx
+++ src/components/ui/starsWrapper.tsx
@@ -1,6 +1,6 @@
 import { Stars } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import "@/components/ui/StarsWrapper.css";
+import "@/components/ui/starsWrapper.css";
 
 export const StarsWrapper = () => {
 	return (
